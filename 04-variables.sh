288dfd84682708f2f1254406e9c42bda61a9f2c0
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-09 22:55:16 +0530

    veriables

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Raheem
+PERSON2=suresh
+echo"$PERSON1:: hi ${PERSON2} suresh how are you"
+echo"${PERSON2}:: hello $PERSON1 i am fine what are you doing?"
+echo"$PERSON1:: i am doing good ${PERSON2} what's going on"?
+echo"${PERSON2}:: i started lerning shell script $PERSON1"
