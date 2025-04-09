cf8df47828641940f9dff3ebe2ac541c3de9211d
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-10 04:22:17 +0530

    veriables

--- 12-installetion.sh
+++ 12-installetion.sh
@@ -1,13 +1,11 @@
-#!/bin/basH
+#!/bin/bash
+
 USERID=$(id -u)
 #echo "user ID is: $USERID"
 
-
 if [ $USERID -ne 0 ]
 then
-echo "please run this script with root priveleges"
-
+     echo "please run this script with root priveleges"
 fi
 
 dnf install git -y
-
