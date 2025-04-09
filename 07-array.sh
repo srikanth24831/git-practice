0705e3be52ecdd3904d58054bdf4964cce278602
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-09 23:49:10 +0530

    veriables

--- 07-array.sh
+++ 07-array.sh
@@ -2,7 +2,7 @@
 
 # index starts from 0, size is 3
 
-fruits ("APPLE" "KIWI" "ORANGE") #Array
+FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
 echo "first fruit is: ${FRUITS[0]}"
 echo "second fruit is: ${FRUITS[1]}"
