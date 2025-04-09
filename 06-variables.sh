b6ecc3c753e872a5ed32932971ef714dbb3ec363
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-09 23:29:59 +0530

    veriables

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "please enter your username::"
 
-read USERNAME
+read -s USERNAME #takes input into USERNAME variable
 
 echo "username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password entered is: $PASSWORD"
