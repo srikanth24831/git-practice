e9324c2bae25d8753f221484ceae8c2ee99de964
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-10 04:48:11 +0530

    veriables

--- 12-installetion.sh
+++ 12-installetion.sh
@@ -9,5 +9,14 @@ then
      exit 1
 fi
 
-dnf install git -y
+dnf installed git
 
+if [ $? -ne 0 ]
+then
+
+      echo "git is not installed going to install it.."
+      dnf install git -y
+else
+
+        echo "git is already installed nothing to do.."
+fi
