d033faa44273462d9e3a7534bcfacfed287fc8c1
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-10 05:56:20 +0530

    veriables

---
+++ 13-functions.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+!/bin/bash
+
+USERID=$(id -u)
+#echo "user ID is: $USERID"
+
+VALIDATE(){
+    echo "exit status:$1"
+
+}
+
+if [ $USERID -ne 0 ]
+then
+     echo "please run this script with root priveleges"
+     exit 1
+fi
+
+dnf installed git
+
+VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then
+#  echo "git is not installed, going to install it.."
+#       dnf install git -y
+#       if [ $? -ne 0 ]
+#       then 
+#            echo "git installation is not success...check it"
+#            exit 1
+#       else
+#            echo "git installation is success"
+#       fi
+# else
+
+#         echo "git is already installed nothing to do.."
+# fi
+
+#   dnf list installed mysql
+  
+#   if [ $? -ne 0 ]
+#   then
+#        echo "mysql is not installed ...going to install"
+#         dnf install mysql -y
+#         if [ $? -ne 0 ]
+#         then
+#              echo "mysql installation is failure..please check"
+#              exit 1
+#         else
+#              echo "mysql installation is success"
+#         fi 
+#     else
+#          echo "mysql is already installed..nothing to do"
+#     fi
