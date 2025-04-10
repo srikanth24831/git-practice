960c78e4ac9c97639fe335f0f849d955161f6d6c
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-10 07:40:46 +0530

    veriables

--- 13-functions.sh
+++ 13-functions.sh
@@ -3,16 +3,25 @@
 USERID=$(id -u)
 #echo "user ID is: $USERID"
 
+CHECK_ROOT(){
+if [ USERID -ne 0 ]
+then
+     echo "please run this script with root priveleges"
+     exit 1
+fi
+
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
          echo "$2 is...FAILED"
          exit 1
     else
-         echo "$2 is...SUCCESS"
+         echo "$2 is...SUCCESS
     fi
-
 }
+   CHECK_ROOT
 
 if [ $USERID -ne 0 ]
 then
