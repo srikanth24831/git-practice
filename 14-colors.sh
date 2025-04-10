cde0b95b27effc5e86f28ebb8b48ed69d83a09a9
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-10 10:57:16 +0530

    veriables

--- 14-colors.sh
+++ 14-colors.sh
@@ -5,30 +5,40 @@ R="\e[31m"
 G=\e[32m"
 
 CHECK_ROOT(){
-if [ $USERID -ne 0 ]
+  if [ $USERID -ne 0 ]
 then
-     echo "please run this script with root priveleges"
+   echo "please run this script with root privilages"
      exit 1
+  fi
+}
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+     echo -e "$2 is...$R SUCCESS $N"
+     exit 1
+else
+     echo -e "$2 is...$G FAILED $N" 
 fi
 
 }
-
-VALIDATE(){
+CHECK_ROOT
+dnf list installed git
     if [ $1 -ne 0 ]
     then
-         echo -e "$2 is...$R FAILED $N"
-         exit 1
-    else
-          echo "$2 is... $G SUCCESS $N"
-    fi
-}
-  CHECK_ROOT
+         echo "Git is not installed, going to install it.."
 
-  dnf list installed git
+         dnf install hit -y
+         VALDATE $? "installing git"
+    else
+          echo "Git is already installed nothing to do"
+          
+    fi
+
+  dnf list installed mysqlsss
   if [ $? -ne 0 ]
   then
        echo "mysql is not installed...going to install"
-       dnf install mysql -y
+       dnf install mysqlsss -y
        VALIDATE $? "installing my sql"
  else 
        echo "mysql is already installed..nothing to do"
