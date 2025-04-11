36e0cc52f0d43a29bf773f13ced29c6bbd043fe8
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-11 23:23:16 +0530

    shell

---
+++ 17-redirectors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILES="$LOGS_FOLDER/SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+R="\e[31m"
+G=\e[32m"
+N=\e[33m"
+
+CHECK_ROOT(){
+  if [ $USERID -ne 0 ]
+then
+   echo -e " $R please run this script with root privilages $N" &>>$LOG_FILE
+     exit 1
+  fi
+}
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+     echo -e "$2 is...$R SUCCESS $N" &>>$LOG_FILE
+     exit 1
+else
+     echo -e "$2 is...$G FAILED $N" &>>$LOG_FILE
+fi
+
+}
+CHECK_ROOT
