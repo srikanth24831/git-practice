431f730629a07657d0353624520a2d8c7c993049
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-19 22:35:11 +0530

    shell

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -21,10 +21,10 @@ then
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-     echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+     echo -e "$2 is...$R FAILED $N" | tee -a &>>$LOG_FILE
      exit 1
 else
-     echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+     echo -e "$2 is...$G SUCCESS $N" | tee -a &>>$LOG_FILE
 fi
 }
 USAGE(){
