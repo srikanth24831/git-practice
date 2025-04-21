203b087ab093da98fbffee83daf6413adf37a555
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-21 20:15:52 +0530

    shell

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -9,7 +9,8 @@ mkdir -p $LOGS_FOLDER
 USERID=$(id -u)
 R="\e[31m"
 G=\e[32m"
-N=\e[33m"
+N=\e[0m"
+Y=\e[33m"
 
 CHECK_ROOT(){
   if [ $USERID -ne 0 ]
@@ -29,3 +30,18 @@ fi
 
 }
 CHECK_ROOT
+
+
+
+for package in $@  # $@ refers to all orguments passed to it
+do
+
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+    echo "$package is not insstalled going to install it.." &>>$LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
+    else
+         echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+     fi    
+    done
