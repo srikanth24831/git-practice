754a9bc7c91ac1b103e0d817ed0dfddde32ae089
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-21 21:06:51 +0530

    shell

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -1,49 +1,30 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FOLDER="/var/log/shell-script"
+SCRIPTNAME=$(echo $0 | cut -d "." -fi)
+TIMESTAMP=$(date +%Y-%M-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
-
 USERID=$(id -u)
 R="\e[31m"
 G=\e[32m"
-N=\e[0m"
-Y=\e[33m"
+N=\e[33m"
 
 CHECK_ROOT(){
   if [ $USERID -ne 0 ]
 then
    echo -e "$R please run this script with root privilages $N" &>>$LOG_FILE
-        exit 1
+     exit 1
   fi
 }
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-     echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+     echo -e "$2 is...$R SUCCESS $N" &>>$LOG_FILE
      exit 1
 else
-     echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+     echo -e "$2 is...$G FAILED $N" &>>$LOG_FILE
 fi
 
 }
-
-
 CHECK_ROOT
-    
-
-for package in $@  # $@ refers to all orguments passed to it
-do
-
-    dnf list installed $package &>>$LOG_FILE
-    if [ $? -ne 0 ]
-    then
-         echo "$package is not insstalled going to install it.." &>>$LOG_FILE
-    dnf install $package -y &>>$LOG_FILE
-    VALIDATE $? "installing package"
-    else
-         echo "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
-     fi    
-    done
