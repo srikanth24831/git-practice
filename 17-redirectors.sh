98a5bf104335a6cd160489a8a4dcd630ed304242
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-21 20:05:58 +0530

    shell

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -3,9 +3,10 @@
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER"/$SCRIPT_NAME-$TIMESTAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
+USERID=$(id -u)
 R="\e[31m"
 G=\e[32m"
 N=\e[33m"
@@ -20,10 +21,10 @@ then
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-     echo -e "$2 is...$R SUCCESS $N" &>>$LOG_FILE
+     echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
      exit 1
 else
-     echo -e "$2 is...$G FAILED $N" &>>$LOG_FILE
+     echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
 fi
 
 }
