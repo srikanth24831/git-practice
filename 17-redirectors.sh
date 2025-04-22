52fb24d61a7b409f158875703103ab0b7e2c1526
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-22 19:44:53 +0530

    shell

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -4,7 +4,7 @@ LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%M-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+mkdir -v $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
